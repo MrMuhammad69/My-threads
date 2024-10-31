9e18c7789bca328130cf930a904309b6316861b9
MrMuhammad69 <ibneabdullatifjud@gmail.com>
2024-10-31 17:41:15 +0500

    Changes Needed for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -7,9 +7,10 @@ import postRoutes from './Routes/post.route.js'
 import protectRoute from "./middleware/protectRoute.js";
 import messageRoutes from "./Routes/message.route.js";
 import {app, server} from "./Socket/socket.js"
+import path from "path";
 dotenv.config();
 connectDB()
-
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json({limit: "50mb"}))
@@ -23,9 +24,18 @@ app.use('/api/posts',protectRoute, postRoutes)
 app.use('/api/messages',protectRoute, messageRoutes)
 
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// http://localhost:5000 -> Backend,Frontend
+ 
+
+// React App
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
+  });
+}
+
+
 server.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
 });
